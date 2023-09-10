4a1a50d3c93a628a5d1d345dc2352fdde3930943
GuardianN06 <104389989+GuardianN06@users.noreply.github.com>
2023-09-10 09:52:14 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Invokerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.FlatAppearance.BorderSize = 50;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Step 1\r\nDownload \r\nPowershell";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(115, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 69);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(12, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(545, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Step 2\r\nSelect program\r\nto run as Invoker";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(569, 185);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Invokerator";
+            this.ResumeLayout(false);
+
+        }
+
+       // private void progressBar1_Click(object sender, EventArgs e) => throw new NotImplementedException();
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+
+namespace Invokerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string downloadUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.zip";
+            string exeDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            string downloadPath = Path.Combine(exeDirectory, "pwsh.zip");
+            string extractionDirectory = Path.Combine(exeDirectory, "pwsh");
+            string pwshExePath = Path.Combine(extractionDirectory, "pwsh.exe");
+
+            if (File.Exists(pwshExePath))
+            {
+                progressBar1.Value = 100;
+                MessageBox.Show("PowerShell is already downloaded and ready to use", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadProgressChanged += (s, args) =>
+                    {
+                        progressBar1.Value = args.ProgressPercentage;
+                    };
+
+                    webClient.DownloadFileCompleted += (s, args) =>
+                    {
+                        if (args.Error == null)
+                        {
+                            try
+                            {
+                                Directory.CreateDirectory(extractionDirectory);
+
+                                ZipFile.ExtractToDirectory(downloadPath, extractionDirectory);
+
+                                MessageBox.Show("Powershell downloaded successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Extraction or execution failed: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Download failed: " + args.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    };
+
+                    try
+                    {
+                        webClient.DownloadFileAsync(new Uri(downloadUrl), downloadPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Download failed: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string exeDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            string extractionDirectory = Path.Combine(exeDirectory, "pwsh");
+            string pwshExePath = Path.Combine(extractionDirectory, "pwsh.exe");
+
+            if (File.Exists(pwshExePath))
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Executable Files|*.exe";
+                    openFileDialog.Title = "Select an .exe file to launch";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string selectedExePath = openFileDialog.FileName;
+                        string powershellCommand = $"$env:__COMPAT_LAYER='RunAsInvoker'; & '{selectedExePath}'";
+                        ProcessStartInfo startInfo = new ProcessStartInfo(pwshExePath, $"-Command {powershellCommand}");
+                        Process.Start(startInfo);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("PowerShell is not downloaded yet. Please download it first.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
